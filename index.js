7f71aedf6314156b402a60edd1690f001744f605
OlgaLiubar <olgaliubar@yandex.ru>
2021-12-11 22:46:32 +0300

    feat: create new campground

--- index.js
+++ index.js
@@ -20,22 +20,43 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
   res.render('home');
 })
 
 app.get('/campgrounds', async (req, res) => {
-const campgrounds = await Campground.find({});
-res.render('campgrounds/index', { campgrounds });
+  const campgrounds = await Campground.find({});
+  res.render('campgrounds/index', {
+    campgrounds
+  });
+})
+
+app.get('/campgrounds/new', (req, res) => {
+  res.render('campgrounds/new');
+})
+
+app.post('/campgrounds', async (req, res) => {
+  // const {title, location} = req.body;
+  // const campground = new Campground({title, location});
+  const campground = new Campground(req.body.campground);
+  //вместо Campground.create()
+  await campground.save();
+  res.redirect(`campgrounds/${campground._id}`);
 })
 
 app.get('/campgrounds/:id', async (req, res) => {
   const campground = await Campground.findById(req.params.id);
-  res.render('campgrounds/show', { campground });
-  })
+  res.render('campgrounds/show', {
+    campground
+  });
+})
+
+
 
 async function main() {
-  await mongoose.connect ('mongodb://localhost:27017/yelp-camp', {
+  await mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
     // useCreateIndex: true,
     // useFindAndModify: false,
