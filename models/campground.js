cdf4ba3fc6ee997f9c23c5059810448509a33959
OlgaLiubar <olgaliubar@yandex.ru>
2021-12-13 17:36:56 +0300

    feat: campground images from Unsplash collection

--- models/campground.js
+++ models/campground.js
@@ -4,8 +4,10 @@ const Schema = mongoose.Schema;
 
 const CampgroundSchema = new Schema({
   title: String,
-  price: String,
+  image: String,
+  price: Number,
   description: String,
   location: String
 });
+
 module.exports = mongoose.model('Campground', CampgroundSchema);
--- seeds/index.js
+++ seeds/index.js
@@ -4,6 +4,7 @@ const cities = require("./cities");
 const { places, descriptors } = require("./seedHelpers");
 
 const db = mongoose.connection;
+
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
   console.log("Database connected");
@@ -15,9 +16,13 @@ const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
+      image: 'https://source.unsplash.com/collection/483251',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
+      price
     });
     await camp.save();
   }
@@ -31,9 +36,9 @@ async function main() {
     useUnifiedTopology: true,
   });
 
-  await app.listen(3000, () => {
-    console.log("Serving on port 3000");
-  });
+  // await app.listen(3000, () => {
+  //   console.log("Serving on port 3000");
+  // });
 }
 
 main();
