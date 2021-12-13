c8d366a7e0d136f13594111a4a42837752e4db65
OlgaLiubar <olgaliubar@yandex.ru>
2021-12-13 16:20:49 +0300

    feat: ejs-mate layout

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
-
-// mongoose.connect('mongodb://localhost:27017/yelp-camp', {
-//   useNewUrlParser: true,
-//   useCreateIndex: true,
-//   useUnifiedTopology: true
-// });
+const morgan = require('morgan');
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
@@ -17,6 +13,10 @@ db.once("open", () => {
 });
 
 const app = express();
+
+app.use(morgan('tiny'));
+
+app.engine('ejs', ejsMate)
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
